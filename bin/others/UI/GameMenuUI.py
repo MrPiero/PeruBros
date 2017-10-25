0f7ef67535ee66d960876272b3662e2e42c68701
MrPiero <pbardelli20@gmail.com>
2017-10-25 09:02:56 -0500

    Animaciones de partidas

--- bin/others/UI/GameMenuUI.py
+++ bin/others/UI/GameMenuUI.py
@@ -4,13 +4,25 @@ class GameUIMenu:
     def __init__(self, idUser):
         pygame.init()
 
-        self.display_width = 1280
-        self.display_height = 720
+        self.display_width = 800
+        self.display_height = 600
+        self.animar = False
+        self.last_update = 0
+        self.current_frame = 0
 
         self.gameMenuDisplay = pygame.display.set_mode((self.display_width, self.display_height))
         pygame.display.set_caption("PERUBROS. -- PRE-ALPHA --")
         self.clock = pygame.time.Clock()
         self.user = self.obtener_info_usuario(idUser)
+
+    def animate(self, action_type):
+        now = pygame.time.get_ticks()
+        if now - self.last_update > 100:
+            self.last_update = now
+            self.current_frame = (self.current_frame + 1) % len(action_type)
+            self.image = action_type[self.current_frame]
+            self.rect = self.image.get_rect()
+            self.rect.topleft = ((300, 100))
 
     def obtener_info_usuario(self, id):
         url = "https://jsonplaceholder.typicode.com/users/" + str(id)
@@ -31,6 +43,9 @@ class GameUIMenu:
         return text_surf, text_rect
 
     def obtener_img_personaje(self, personaje):
+        self.imagenes = []
+        self.imagenes.append(pygame.image.load('./resources/nino.png'))
+        self.imagenes.append(pygame.image.load('./resources/nina.png'))
         personajeImg = None
         if personaje == 1 : personajeImg = pygame.image.load('./resources/nino.png')
         elif personaje == 2 : personajeImg = pygame.image.load('./resources/nina.png')
@@ -67,9 +82,12 @@ class GameUIMenu:
             self.gameMenuDisplay.blit(partida3, (0, partida3_rec))
 
             if pygame.mouse.get_pos()[1] >= partida1_rec and pygame.mouse.get_pos()[1] < partida2_rec:
-                self.gameMenuDisplay.blit(self.obtener_img_personaje(1), (300, 100))
+                self.animate(self.imagenes)
+                self.gameMenuDisplay.blit(self.image, self.rect)
+                self.animar = True
             elif pygame.mouse.get_pos()[1] >= partida2_rec and pygame.mouse.get_pos()[1] < partida3_rec:
                 self.gameMenuDisplay.blit(self.obtener_img_personaje(2), (300, 100))
+                self.animar = True
             elif pygame.mouse.get_pos()[1] >= partida3_rec:
                 self.gameMenuDisplay.blit(self.obtener_img_personaje(1), (300, 100))
 
