d1709ebcc06afa70ee6e47a76d937a56b924cb61
Piero Bardelli <pbardelli20@gmail.com>
2017-11-26 23:09:36 -0500

    Added test_seleccion_nivel_1_1

--- unittest_perubros.py
+++ unittest_perubros.py
@@ -1,5 +1,6 @@
 import unittest
 from bin.others.UI.LoginUI import LoginUIMenu
+from bin.others.UI.LevelMenuUI import LevelUIMenu
 from bin.game import main as game
 from bin.others.levelReader import uncode as lv_read
 
@@ -27,3 +28,7 @@ class TestBasico(unittest.TestCase):
         a = lv_read('lvl_x_x')
         self.assertEqual([[[(70, 0, 70, 70), 50, 550], [(70, 0, 70, 70), -20, 550], [(70, 0, 70, 70), 50, 480], [(70, 0, 70, 70), -20, 480], [(70, 0, 70, 70), 50, 410], [(70, 0, 70, 70), -20, 410], [(70, 0, 70, 70), 50, 340], [(70, 0, 70, 70), -20, 340], [(70, 0, 70, 70), 50, 270], [(70, 0, 70, 70), -20, 270], [(70, 0, 70, 70), 50, 200], [(70, 0, 70, 70), -20, 200], [(70, 0, 70, 70), 50, 130], [(70, 0, 70, 70), -20, 130], [(70, 0, 70, 70), 50, 60], [(70, 0, 70, 70), -20, 60], [(70, 0, 70, 70), 50, -10], [(70, 0, 70, 70), -20, -10], [(70, 0, 70, 70), 50, -80], [(70, 0, 70, 70), -20, -80], [(0, 0, 70, 70), 500, 490], [(70, 140, 70, 70), 120, 550], [(70, 140, 70, 70), 190, 550], [(70, 0, 70, 70), 330, 340]], [], [], 500] ,a)
 
+    def test_seleccion_nivel_1_1(self):
+        LevelMenu = LevelUIMenu(4)
+        progress = LevelMenu.main_menu()
+        self.assertEqual((1, 1), progress)
