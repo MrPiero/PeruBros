c7fc5b201c41447cf8811b37d60583a959efd53d
jagerod <rodrigomejia@icloud.com>
2017-11-27 02:03:50 -0500

    ???

--- start.py
+++ start.py
@@ -36,7 +36,7 @@ elif sys.argv[1] == 'test1':
     LevelMenu = LevelUIMenu()
     LevelMenu.main_menu()
 elif sys.argv[1] == 'test2':
-    g = game((1,2))
+    g = game((1,1))
     print(g[1])
 elif sys.argv[1] == 'test3':
     t()
